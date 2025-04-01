e47345e8f20ff8ad064a7ca4999c03396b387e3d
Simen Gimsøy <simgims@hotmail.com>
2025-04-01 13:23:50 +0200

    Setup of Heroku

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,28 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-import pg from "pg";
 import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
 
 const app = new Hono();
 
-/*
-const postgresql = new pg.Pool({ user: "postgres" });
+const connectionString = process.env.DATABASE_URL;
+const postgresql = connectionString
+  ? new pg.Pool({ connectionString, ssl: { rejectUnauthorized: false } })
+  : new pg.Pool({ user: "postgres" });
+
+// const postgresql = new pg.Pool({ user: "postgres", password: "postgres" }); // Gammel versjon
 const latitudeLongitude = { type: "name", properties: { name: "ESPG:4326" } };
 
 app.get("/api/skoler", async (c) => {
-  const result = await postgresql.query(//TODO);
+  const result = await postgresql.query(
+    "select skolenavn, st_transform(skole.posisjon, 4326)::json as posisjon\n" +
+      "from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole skole\n" +
+      "         inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke fylke\n" +
+      "                    on st_contains(fylke.omrade, skole.posisjon)\n" +
+      "         inner join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn navn\n" +
+      "                    on fylke.objid = navn.fylke_fk and navn.sprak = 'nor'\n" +
+      "where navn.navn = 'Viken'",
+  );
   return c.json({
     type: "FeatureCollection",
     crs: latitudeLongitude,
@@ -24,11 +36,8 @@ app.get("/api/skoler", async (c) => {
   });
 });
 
-*/
-
-app.get("/api/hello", async (c) => {
-  return c.text("Hello World!");
-});
 app.use("*", serveStatic({ root: "../dist" }));
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-serve({ ...app, port });
+serve({
+  fetch: app.fetch,
+  port: process.env.port ? parseInt(process.env.port) : 3000,
+});
