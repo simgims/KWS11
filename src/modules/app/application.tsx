8949f48fde602a1e751c67c90086ec1ab8a86086
Simen Gimsøy <simgims@hotmail.com>
2025-04-01 14:40:14 +0200

    Setting up click on the points

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,26 +1,17 @@
 import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import { Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
-
-// Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 
-// By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
-//  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
 
-// Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
-//  map data structure will be used
 const map = new Map({
-  // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
   layers: [
     new TileLayer({ source: new OSM() }),
     new VectorLayer({
@@ -29,16 +20,21 @@ const map = new Map({
   ],
 });
 
-// A functional React component
 export function Application() {
-  // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
-  // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
-  // map React component
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
+
+    map.on("singleclick", (evt: MapBrowserEvent<MouseEvent>) => {
+      map.forEachFeatureAtPixel(evt.pixel, (feature) => {
+        const schoolName = feature.get("skolenavn");
+        if (schoolName) {
+          alert(`School: ${schoolName}`);
+        }
+      });
+    });
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
   return <div ref={mapRef}></div>;
 }
